672696530ef5111459b2f2820cba1365f5a3482b
WinterSolstice8 <60417494+WinterSolstice8@users.noreply.github.com>
2022-08-27 00:58:30 -0700

    [lua] [zones] Add MEMBERS_LEVELS_ARE_RESTRICTED message id to all zones

--- scripts/zones/Residential_Area/IDs.lua
+++ scripts/zones/Residential_Area/IDs.lua
@@ -17,6 +17,7 @@ zones[xi.zone.RESIDENTIAL_AREA] =
         CARRIED_OVER_POINTS     = 7000, -- You have carried over <number> login point[/s].
         LOGIN_CAMPAIGN_UNDERWAY = 7001, -- The [/January/February/March/April/May/June/July/August/September/October/November/December] <number> Login Campaign is currently underway!<space>
         LOGIN_NUMBER            = 7002, -- In celebration of your most recent login (login no. <number>), we have provided you with <number> points! You currently have a total of <number> points.
+        MEMBERS_LEVELS_ARE_RESTRICTED = 0, -- Your party is unable to participate because certain members' levels are restricted.
     },
     mob =
     {
